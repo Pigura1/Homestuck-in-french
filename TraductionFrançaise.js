6cb5c5be5f35d0ed00a6c07d7007eb092f1573e6
Pigura1 <C.victor2006@laposte.net>
2025-10-03 10:35:57 +0200

    version 0.1 up to page 10 of Homestuck

---
+++ TraductionFrançaise.js
@@ -0,0 +1,58 @@
+module.exports = {
+    title: "Homestuck en français",
+    summary: "Ce mod est une traduction française de HomeStuck /This mod is a french translation of Homestuck",
+    description: "Ce mod reprends la traduction de stoubs et la retranspose dans ce mod / This takes the translation by stoubs and redispose it into this mod ",
+
+    author: "Pigura",
+    version: 0.1,
+
+    edit: true,
+
+
+    edit(archive) {
+        archive.mspa.story['001901'].content =
+
+            `Un jeune homme se tient dans sa chambre. Aujourd’hui, le 13 avril 2009, c’est son anniversaire, 
+    et bien que cela fasse treize longues années qu’il ait vu le jour, ce n’est qu’aujourd’hui qu’il aura enfin un nom !
+        <br /><br />
+        Alors, quel sera son nom ?`
+        archive.mspa.story['001904'].content =
+            `Votre nom est JOHN, comme précédemment mentionné aujourd'hui c’est votre ANNIVERSAIRE. Plusieurs GÂTEAUX sont disséminés au travers de votre chambre. Vous avez un certain nombre de CENTRES D’INTÉRÊT. Vous êtes passionné par les TRÈS MAUVAIS FILMS. Vous aimez la programmation informatique mais vous n’y êtes PAS TRÈS BON. Vous vous intéressez également au PARANORMAL et aspirez à devenir MAGICIEN AMATEUR. Vous aimez également jouer à des JEUX de temps en temps.
+        <br /><br />
+        Qu’allez-vous faire ?
+      
+      `
+        archive.mspa.story['001905'].content =
+            `Vos BRAS sont dans votre COFFRE MAGIQUE Pooplord !`
+        archive.mspa.story['001906'].content =
+            `Attendri par le manque apparent de bras du pauvre John, vous prenez le GÂTEAU et le déposez sur son LIT.`
+        archive.mspa.story['001907'].content =
+            `
+Vous récupérez vos FAUX BRAS de votre coffre magique. Ils vous sont utiles lors de vos BOUFFONNERIES HILARANTES.
+<br /><br />
+Vous les CAPTCHALOGUEZ dans votre SYLLADEX, même si vous n’avez pas la moindre idée de ce que ça peut bien vouloir dire.
+<br /><br />
+Le coffre contient d’autres objets`
+        archive.mspa.story['001908'].content =
+            `C’est là que vous entreposez votre vaste gamme d’ARTEFACTS mystique, chacun d’entre eux pouvant potentiellement devenir une arme dévastatrice dans les mains d’un MAGICIEN EXPÉRIMENTÉ ou d’un FARCEUR AUDACIEUX.
+<br /><br />
+Vous n’êtes aucun des deux.
+<br /><br />
+Parmi les ARTEFACTS se trouvent : DEUX (2) FAUX BRAS [ACTUELLEMENT CAPTCHALOGUÉ DANS VOTRE SYLLADEX], UNE (1) PAIRE DE FAUSSES MENOTTES, UNE (1) ÉPÉE FACTICE, UN (1) CHAPEAU DE MAGICIEN, UNE (1) PAIRE DE LUNETTES A MOUSTACHE, PLUSIEURS (~) CAPSULES À FUMÉE, PLUSIEURS (~) CAPSULES DE FAUX SANG, UNE (1) COPIE DU REDOUTABLE TRAITE DES FRIVOLITÉS MAGIQUES ET AUTRE BLAGUES PRATIQUES PAR LE COLONEL SASSACRE et UNE (1) COPIE DU "GROS MALIN" HARRY ANDERSON, DE MIKE CAVENEY.
+<br /><br />
+Certains de ces objets finiront sans doute par être utiles à un moment ou à un autre, mais pour l’instant vous décidez de n’emporter que les CAPSULES À FUMÉE.`
+        archive.mspa.story['001909'].content =
+            `Vous stocker les CAPSULES À FUMÉE dans une des CARTES CAPTCHALOGUES de votre SYLLADEX.
+<br /><br />
+Vous n’êtes toujours pas tout à fait sûr de ce que cela signifie, mais vous commencez à vous habituer au jargon.
+<br /><br />
+Il vous reste deux CARTES CAPTCHALOGUES.`
+
+archive.mspa.story['001910'].content =
+`Vous n’êtes pas tout à fait sûr que "ÉQUIPER" soit un verbe compatible avec le comportement abstrait que semble exhiber le medium dans lequel vous êtes coincé, mais vous décidez quand même de tenter le coup.
+<br /><br />
+Malheureusement vous ne pouvez pas accéder à vos FAUX BRAS ! Leur carte est coincée sous celle que vous venez d’utiliser pour captchaloguer les     CAPSULES À FUMÉE. Vous allez devoir utiliser les capsules avant de pouvoir accéder à vos bras. Un geste peu avisé qui ne servirait qu’à enfumer votre chambre.
+<br /><br />
+Le MODUS qui suit actuellement votre SYLLADEX est réglé sur une logique de STRUCTURE D’EMPILAGE DES DONNÉES. Vous n’avez jamais été bon avec les structures de données et avez toujours trouvé le concept compliqué et légèrement agaçant.`
+    }
+}
